de4c52fcc6c332c0d4f7bdd78eb8f7e379597349
Mert Gürer <mert.gurer@hotmail.com.tr>
2023-02-11 17:07:21 +0300

    update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,44 +1,48 @@
+import { VerticalTimeline } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 import { ExperienceBox, Type } from "./ExperienceBox";
 
 export const Experience = () => {
   return (
     <section className="experience" id="experience">
-      <h1 className="experience-header">My Experiences</h1>
       <div className="experience-tree">
-        <ExperienceBox
-          title={"Burak Bora High School / Istanbul"}
-          description={"Four year's of High School education"}
-          years={"2015 - 2019"}
-          type={Type.education}
-        />
-        <ExperienceBox
-          title={"IJDG / Germany"}
-          description={
-            "Ran a house full of volunteers. Sailed and camped in the wildlife. Learned how to live in nature."
-          }
-          years={"2016"}
-          type={Type.volunteering}
-        />
-        <ExperienceBox
-          title={"IJDG / Germany"}
-          description={
-            "Maintenance of the storage places, construction of a prefabricated house for a children's summer camp"
-          }
-          years={"2016"}
-          type={Type.volunteering}
-        />
-        <ExperienceBox
-          title={"AFS Exchange Student / Dominican Republic"}
-          description={"Computer Science Engineering student"}
-          years={"2019"}
-          type={Type.education}
-        />
-        <ExperienceBox
-          title={"Gebze Technical University / Kocaeli"}
-          description={"Computer Science Engineering student"}
-          years={"2019 - present"}
-          type={Type.education}
-        />
+        <h1 className="experience-header">Experiences</h1>
+        <VerticalTimeline lineColor="#3b5249">
+          <ExperienceBox
+            title={"Burak Bora High School / Istanbul"}
+            description={"Four year's of High School education"}
+            date={"2015 - 2019"}
+            type={Type.education}
+          />
+          <ExperienceBox
+            title={"IJDG / Germany"}
+            description={
+              "Ran a house full of volunteers. Sailed and camped in the wildlife. Learned how to live in nature."
+            }
+            date={"2016 - 2 weeks"}
+            type={Type.volunteering}
+          />
+          <ExperienceBox
+            title={"IJDG / Germany"}
+            description={
+              "Maintenance of the storage places, construction of a prefabricated house for a children's summer camp"
+            }
+            date={"2018 - 2 weeks"}
+            type={Type.volunteering}
+          />
+          <ExperienceBox
+            title={"AFS Exchange Student / Dominican Republic"}
+            description={"Computer Science Engineering student"}
+            date={"2019 - 2020"}
+            type={Type.education}
+          />
+          <ExperienceBox
+            title={"Gebze Technical University / Kocaeli"}
+            description={"Computer Science Engineering student"}
+            date={"2019 - present"}
+            type={Type.education}
+          />
+        </VerticalTimeline>
       </div>
     </section>
   );
--- src/components/ExperienceBox.tsx
+++ src/components/ExperienceBox.tsx
@@ -1,10 +1,12 @@
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+
 import { IoMdSchool } from "react-icons/io";
 import { MdWork, MdVolunteerActivism } from "react-icons/md";
 
 interface props {
   title: string;
   description: string;
-  years: string;
+  date: string;
   type: Type;
 }
 
@@ -16,24 +18,24 @@ export enum Type {
 
 const chooseType = (type: string) => {
   if (type === Type.education) {
-    return <IoMdSchool size={43} />;
+    return <IoMdSchool />;
   } else if (type === Type.volunteering) {
-    return <MdVolunteerActivism size={43} />;
+    return <MdVolunteerActivism />;
   } else if (type === Type.work) {
-    return <MdWork size={43} />;
+    return <MdWork />;
   }
 };
 
 export const ExperienceBox = (props: props) => {
   return (
-    <div className="experience-field">
-      <div className="experience-box">
-        <h1 style={{ paddingBottom: "10px" }}>{props.title}</h1>
-        <span>{props.description}</span>
-      </div>
-      <div className="experience-logo">{chooseType(props.type)}</div>
-
-      <span style={{ flex: "1" }}>{props.years}</span>
-    </div>
+    <VerticalTimelineElement
+      className=""
+      date={props.date}
+      iconStyle={{ background: "#3b5249" }}
+      icon={chooseType(props.type)}
+    >
+      <h3 className="">{props.title}</h3>
+      <p className="">{props.description}</p>
+    </VerticalTimelineElement>
   );
 };
